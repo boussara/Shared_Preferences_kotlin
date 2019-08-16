b29de568a699e9f1689cfacb42f7d7a2f49a79c0
Mona Lisa <Mona@gmail.com>
2019-08-16 13:48:20 +0100

    Notifications

--- app/src/main/java/com/example/mohamedessakotlin/Notifications.kt
+++ app/src/main/java/com/example/mohamedessakotlin/Notifications.kt
@@ -1,12 +1,51 @@
 package com.example.mohamedessakotlin
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Notification
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Notifications : AppCompatActivity() {
+
+
+    lateinit var btn : Button
+    companion object {
+        var id = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notifications)
+
+        btn = findViewById(R.id.btn)
+    }
+
+    fun notifications(view: View){
+
+        val intent = Intent()
+        val pendingIntent = PendingIntent.getActivity(this@Notifications,0,intent,0)
+        val notification = Notification.Builder(this@Notifications)
+            .setSmallIcon(R.drawable.notification_icon_background)
+            .setContentTitle("Muhammed Essa")
+            .setContentText("Hello Body")
+        notification.setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        notificationManager.notify(id,notification.build())
+        id++
+
+    }
+
+    fun previous_but(view: View){
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
     }
 }
